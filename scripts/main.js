d0c3f3a0e2e00d6fe99c0fc8632d9f8b6b228dfb
Hali Morelli <morellih@ufl.edu>
2018-12-02 18:29:48 -0700

    added javascript array, object, and while loop

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+var members = [
+    'Dave Grohl',
+    'Nate Mendel',
+    'Pat Smear',
+    'Taylor Hawkins',
+    'Rami Jaffee',
+    'Chris Shiflett'
+];
+
+var tour = {
+    chicago: 'July 29th & 30th, 2018',
+    seattle: 'September 1st, 2018',
+    edmonton: 'September 4th, 2018'
+};
+
+var i = 0;
+
+while (i < 5) {
+  console.log('Times running through the loop: ' + i);
+
+    i++;
+}
